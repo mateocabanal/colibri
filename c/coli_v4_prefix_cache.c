065825e4452e6be202af20c43ee3784f6bfacafd
Mateo Cabanal <mateo200525@gmail.com>
2026-08-18 18:05:48 -0700

    feat(v4): compose prefix-hot planner adapter

--- c/coli_v4_prefix_cache.c
+++ c/coli_v4_prefix_cache.c
@@ -2,4 +2,5 @@
  * one already-linked translation unit. The RAM implementation remains a
  * byte-for-byte copy of the previously reviewed source. */
 #include "coli_v4_prefix_cache_impl.inc"
+#include "coli_v4_prefix_hot_planner.inc"
 #include "coli_v4_prefix_disk_shared.inc"
